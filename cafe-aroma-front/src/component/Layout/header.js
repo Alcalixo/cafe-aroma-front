be4c9bf48d003e2d47b0dc13d9a9b53ac7558883
Alcalixo <rodrix_gonzalo@outlook.com>
2024-11-19 19:46:06 -0300

    creado Header, con fondo y logo provisional, funcionando correctamente

--- cafe-aroma-front/src/component/Layout/header.js
+++ cafe-aroma-front/src/component/Layout/header.js
@@ -1,2 +1,16 @@
 import React from "react";
-import './header.css'
+import "./header.css";
+import { Container } from "react-bootstrap";
+import logo from "../../assets/img/logo-cafe-aroma.png"; //logo provisional
+
+function Header() {
+  return (
+    <div className="Header">
+      <Container>
+        <img className="Logo" src={logo} alt="Logo Café Aroma" />
+      </Container>
+    </div>
+  );
+}
+
+export default Header;
