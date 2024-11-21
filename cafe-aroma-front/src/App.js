fb7612ce76bbd9e3fe09a1ef84ceec8fdd47d24d
Alcalixo <rodrix_gonzalo@outlook.com>
2024-11-21 19:20:48 -0300

    agregado Navbar, por el momento solo estructura, una vez creada las demas vistas y funcionalidades, se rutearan los distintos botones y links / Ajuste al Footer que impedía que se vea el scroll del navbar

--- cafe-aroma-front/src/App.js
+++ cafe-aroma-front/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./component/Layout/header";
 import Footer from "./component/Layout/footer";
+import NavbarCafe from "./component/Layout/navbar";
 
 function App() {
   return (
     <>
       <Header />
+      <NavbarCafe />
       <p>Welcome</p>
       <Footer />
     </>
--- cafe-aroma-front/src/component/Layout/navbar.js
+++ cafe-aroma-front/src/component/Layout/navbar.js
@@ -1,2 +1,56 @@
 import React from "react";
 import './navbar.css'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { IoHome } from "react-icons/io5";
+import { GiCoffeeCup } from "react-icons/gi";
+import { BiSolidOffer } from "react-icons/bi";
+
+
+function NavbarCafe() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#"><IoHome /> Inicio
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#"><GiCoffeeCup />
+             Menú</Nav.Link>
+            <Nav.Link href="#"><BiSolidOffer />
+             Promos</Nav.Link>
+            <NavDropdown title="Sobre Nosotros" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#">Quienes somos</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#">
+                Contactanos!
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#">Login</Nav.Link>
+          <Nav.Link href="#">Registro</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-danger">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarCafe;
