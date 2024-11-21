2fdb2763a60de4216bca19b1c6e399ef562f8f44
Alcalixo <rodrix_gonzalo@outlook.com>
2024-11-21 18:09:08 -0300

    Terminado footer, instalación de react-icons para implementarlos donde haga falta, links a redes sociales a la pagina de inicio de cada una, luego se realizará el ruteo a las paginas cuando las tengamos construidas

--- cafe-aroma-front/src/App.js
+++ cafe-aroma-front/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./component/Layout/header";
+import Footer from "./component/Layout/footer";
 
 function App() {
   return (
     <>
       <Header />
       <p>Welcome</p>
+      <Footer />
     </>
   );
 }
