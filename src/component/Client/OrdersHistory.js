58078a0c2890ab353550cf82f60064fc60ed9a17
Fernando Corrales <corrales_fernando@hotmail.com>
2024-12-16 14:17:46 -0300

    add cart to table OrdersHistory

--- src/component/Client/OrdersHistory.js
+++ src/component/Client/OrdersHistory.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Table, Button } from "react-bootstrap";
+import { Button, Card, Container, Row, Table } from "react-bootstrap";
 import { useAuth } from "../../service/AuthContext";
 
 export default function OrdersHistory() {
@@ -28,80 +28,105 @@ export default function OrdersHistory() {
     fetchOrdenes();
   }, [user]);
 
-  const ordenesOrdenadas = ordenes.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+  const ordenesOrdenadas = ordenes.sort(
+    (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+  );
 
   const handleRowClick = (order) => {
     setSelectedOrder(order);
   };
 
   return (
-    <div className="container-fluid">
-      <div className="row mt-3">
+    <Container className="mt-3 pt-3">
+      <Row>
         <div className="col-12 col-lg-8 offset-0 offset-lg-2">
-          <div className="table-responsive">
-            <Table bordered>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th className="text-center">NRO ORDEN</th>
-                  <th className="text-center">FECHA</th>
-                  <th className="text-center">STATUS</th>
-                  <th className="text-center">TOTAL</th>
-                  <th className="text-center">ACCIÓN</th>
-                </tr>
-              </thead>
-              <tbody className="table-group-divider">
-                {ordenesOrdenadas.map((order, i) => (
-                  <tr key={order._id} onClick={() => handleRowClick(order)}>
-                    <td>{i + 1}</td>
-                    <td>{order.nro_orden}</td>
-                    <td>{new Date(order.createdAt).toLocaleString()}</td>
-                    <td>{order.status}</td>
-                    <td>
-                      ${(order.items.reduce((acc, item) => acc + item.precio, 0) * 1.21).toFixed(2)}
-                    </td>
-                    <td className="text-center">
-                      <Button variant="primary" onClick={() => handleRowClick(order)}>
-                        Ver Detalle
-                      </Button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-          </div>
-          {selectedOrder && (
-            <div className="mt-3 table-responsive">
+          <Card className="text-center" border="warning">
+            <Card.Header>
+              <h4>Historial de Compras</h4>
+            </Card.Header>
+            <div className="table-responsive">
               <Table bordered>
                 <thead>
                   <tr>
-                    <th className="text-center">IMAGEN</th>
-                    <th className="text-center">NOMBRE</th>
-                    <th className="text-center">CANTIDAD</th>
-                    <th className="text-center">PRECIO + IVA</th>
+                    <th>#</th>
+                    <th className="text-center">NRO ORDEN</th>
+                    <th className="text-center">FECHA</th>
+                    <th className="text-center">STATUS</th>
+                    <th className="text-center">TOTAL</th>
+                    <th className="text-center">ACCIÓN</th>
                   </tr>
                 </thead>
-                <tbody>
-                  {selectedOrder.items.map((item, j) => (
-                    <tr key={j}>
-                      <td style={{ textAlign: "center" }}>
-                        <img
-                          src={item.product_id.img}
-                          alt={item.product_id.name}
-                          style={{ width: "50px" }}
-                        />
+                <tbody className="table-group-divider">
+                  {ordenesOrdenadas.map((order, i) => (
+                    <tr key={order._id} onClick={() => handleRowClick(order)}>
+                      <td>{i + 1}</td>
+                      <td>{order.nro_orden}</td>
+                      <td>{new Date(order.createdAt).toLocaleString()}</td>
+                      <td>{order.status}</td>
+                      <td>
+                        $
+                        {(
+                          order.items.reduce(
+                            (acc, item) => acc + item.precio,
+                            0
+                          ) * 1.21
+                        ).toFixed(2)}
                       </td>
-                      <td>{item.product_id.name}</td>
-                      <td>{item.cantidad}</td>
-                      <td>${(item.precio * 1.21).toFixed(2)}</td>
+                      <td className="text-center">
+                        <Button
+                          variant="primary"
+                          onClick={() => handleRowClick(order)}
+                        >
+                          Ver Detalle
+                        </Button>
+                      </td>
                     </tr>
                   ))}
                 </tbody>
               </Table>
             </div>
+          </Card>
+        </div>
+      </Row>
+      <Row className="mt-3">
+        <div className="col-12 col-lg-8 offset-0 offset-lg-2">
+          {selectedOrder && (
+            <Card className="text-center" border="warning">
+              <Card.Header>
+                <h4>Detalle de la Compra</h4>
+              </Card.Header>
+              <div className="mt-3 table-responsive">
+                <Table bordered>
+                  <thead>
+                    <tr>
+                      <th className="text-center">IMAGEN</th>
+                      <th className="text-center">NOMBRE</th>
+                      <th className="text-center">CANTIDAD</th>
+                      <th className="text-center">PRECIO + IVA</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {selectedOrder.items.map((item, j) => (
+                      <tr key={j}>
+                        <td style={{ textAlign: "center" }}>
+                          <img
+                            src={item.product_id.img}
+                            alt={item.product_id.name}
+                            style={{ width: "50px" }}
+                          />
+                        </td>
+                        <td>{item.product_id.name}</td>
+                        <td>{item.cantidad}</td>
+                        <td>${(item.precio * 1.21).toFixed(2)}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+              </div>
+            </Card>
           )}
         </div>
-      </div>
-    </div>
+      </Row>
+    </Container>
   );
 }
