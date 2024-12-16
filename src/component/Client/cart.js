52e8f0c3a8ed2538fe841b8e941ee660038eae58
Fernando Corrales <corrales_fernando@hotmail.com>
2024-12-16 14:48:53 -0300

    basic styling with cards in cart component

--- src/component/Client/cart.js
+++ src/component/Client/cart.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Form,
+  Image,
+  Row,
+} from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import {
   removeFromCart,
   updateQuantity,
 } from "../../service/Redux/reducers/cartSlice";
-import { Container, Row, Col, Image, Button, Form } from "react-bootstrap";
-import "./cart.css";
-import { Link } from "react-router-dom";
 import ButtonMercadoPago from "./ButtonMercadoPago";
+import "./cart.css";
 
 function Cart() {
   const dispatch = useDispatch();
@@ -30,55 +38,71 @@ function Cart() {
   }
 
   return (
-    <Container className="cart-container">
-      <h2>Tu Carrito</h2>
+    <Container className="mt-3 pt-3">
       <Row className="cart-items">
-        {cart.length === 0 ? (
-          <p>No tienes productos en tu carrito.</p>
-        ) : (
-          cart.map((item) => (
-            <Row key={item._id} className="cart-item align-items-center mb-3">
-              <Col xs={3}>
-                <Image src={item.img} alt={item.name} fluid />
-              </Col>
-              <Col xs={6}>
-                <h4>{item.title}</h4>
-                <p>Precio: ${item.precio}</p>
-              </Col>
-              <Col xs={2}>
-                <Form.Control
-                  type="number"
-                  min="1"
-                  value={item.quantity}
-                  onChange={(e) =>
-                    handleQuantityChange(item._id, e.target.value)
-                  }
-                />
-              </Col>
-              <Col xs={1}>
-                <Button variant="danger" onClick={() => handleRemove(item._id)}>
-                  Eliminar
-                </Button>
-              </Col>
-            </Row>
-          ))
-        )}
+        <Card>
+          <Card.Header>
+            <h2>Tu Carrito</h2>
+          </Card.Header>
+          {cart.length === 0 ? (
+            <p>No tienes productos en tu carrito.</p>
+          ) : (
+            cart.map((item) => (
+              <Row key={item._id} className="cart-item align-items-center mb-3">
+                <Col xs={3}>
+                  <Image
+                    src={item.img}
+                    alt={item.name}
+                    style={{ width: "50px" }}
+                    fluid
+                  />
+                </Col>
+                <Col xs={6}>
+                  <h4>{item.title}</h4>
+                  <p>Precio: ${item.precio}</p>
+                </Col>
+                <Col xs={2}>
+                  <Form.Control
+                    type="number"
+                    min="1"
+                    value={item.quantity}
+                    onChange={(e) =>
+                      handleQuantityChange(item._id, e.target.value)
+                    }
+                  />
+                </Col>
+                <Col xs={1}>
+                  <Button
+                    variant="danger"
+                    onClick={() => handleRemove(item._id)}
+                  >
+                    Eliminar
+                  </Button>
+                </Col>
+              </Row>
+            ))
+          )}
+        </Card>
       </Row>
-      <div className="cart-summary">
-        <h3>Resumen del Pedido</h3>
-        <p>Subtotal: ${calculateSubtotal()}</p>
-        <p>IVA: ${(calculateSubtotal() * 0.21).toFixed(2)} (21%)</p>
-        <h4>Total: ${(calculateSubtotal() * 1.21).toFixed(2)}</h4>
-        <div className="cart-actions mt-4">
-          <Link to="../productos">
-            <Button variant="secondary" className="me-3">
-              Continuar Comprando
-            </Button>
-          </Link>
-          {/* <Button variant="primary">Ir a Pagar</Button> */}
-          <ButtonMercadoPago cart={cart} />
-        </div>
-      </div>
+      <Row>
+        <Card>
+          <Card.Header>
+            <h3>Resumen del Pedido</h3>
+          </Card.Header>
+          <p>Subtotal: ${calculateSubtotal()}</p>
+          <p>IVA: ${(calculateSubtotal() * 0.21).toFixed(2)} (21%)</p>
+          <h4>Total: ${(calculateSubtotal() * 1.21).toFixed(2)}</h4>
+          <div className="cart-actions mt-4">
+            <Link to="../productos">
+              <Button variant="secondary" className="me-3">
+                Continuar Comprando
+              </Button>
+            </Link>
+            {/* <Button variant="primary">Ir a Pagar</Button> */}
+            <ButtonMercadoPago cart={cart} />
+          </div>
+        </Card>
+      </Row>
     </Container>
   );
 }
