d6758de3a38b8b3b13a29805759c7b3d01a19ab3
Alcalixo <rodrix_gonzalo@outlook.com>
2024-12-16 19:04:10 -0300

    pagina error 404 personalizada terminada

--- src/component/Layout/error404.js
+++ src/component/Layout/error404.js
@@ -1,12 +1,34 @@
 import React from "react";
+import { Button, Container, Row } from "react-bootstrap";
+import { GiCoffeeCup } from "react-icons/gi";
+import { Link } from "react-router-dom";
+import './error404.css'
 
-function Erro404 (){
-    return (
-        <div>
-        <h2>Error 404</h2>
-        <p>Página no encontrada</p>
-        </div>
-    )
+function Erro404() {
+  return (
+    <Container className="error404">
+  <Row className="justify-content-center">
+    <GiCoffeeCup size={100} /> {/* Ícono de café grande */}
+  </Row>
+  <Row className="justify-content-center">
+    <h1>Error 404</h1>
+  </Row>
+  <Row className="justify-content-center">
+    <p>No pudimos encontrar lo que buscas</p>
+  </Row>
+  <Row className="justify-content-center">
+    <p>¿Y si mejor te pides un Café?</p>
+  </Row>
+  <Row className="justify-content-center">
+    <Link to="/productos">
+      <Button variant="warning" className="view-products-button">
+        <GiCoffeeCup/>
+      </Button>
+    </Link>
+  </Row>
+</Container>
+
+  );
 }
 
 export default Erro404;
