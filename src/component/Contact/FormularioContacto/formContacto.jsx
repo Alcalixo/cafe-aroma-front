30437d86261ad45d55af62c5d8eea1328c4a5e15
niic0o <nicosena10@gmail.com>
2024-12-17 21:18:05 -0300

    validaciones modal a contactenos

--- src/component/Contact/FormularioContacto/formContacto.jsx
+++ src/component/Contact/FormularioContacto/formContacto.jsx
@@ -1,5 +1,17 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Swal from "sweetalert2"; // Asegúrate de importar Swal
+import withReactContent from "sweetalert2-react-content"; // Asegúrate de importar withReactContent
+
+// Función para mostrar alertas
+export function show_alerta(mensaje, icono) {
+  const mySwal = withReactContent(Swal);
+  mySwal.fire({
+    title: mensaje,
+    icon: icono,
+    confirmButtonText: 'Aceptar'
+  });
+}
 
 function FormContacto() {
   const [form, setForm] = useState({ email: "", asunto: "", descripcion: "" });
@@ -31,25 +43,23 @@ function FormContacto() {
     e.preventDefault();
     try {
       await axios.post(`${process.env.REACT_APP_API_BASE_URL}/api/comentarios/crearComentario`, form);
+      show_alerta("Comentario enviado exitosamente.", "success"); // Mostrar alerta de éxito
       setEnviado(true);
     } catch (error) {
       if (error.response) {
-        // La solicitud se realizó y el servidor respondió con un código de estado que no está en el rango de 2xx
         const errorMessage = error.response.data.error || "Hubo un problema al enviar el mensaje. Por favor, intenta nuevamente.";
-        alert(`Error: ${errorMessage}`);
+        show_alerta(errorMessage, "error"); // Mostrar error en el modal
       } else if (error.request) {
-        // La solicitud se realizó pero no se recibió respuesta
-        alert("No se recibió respuesta del servidor. Por favor, intenta nuevamente.");
+        show_alerta("No se recibió respuesta del servidor. Por favor, intenta nuevamente.", "error");
       } else {
-        // Algo sucedió al configurar la solicitud que lanzó un error
-        alert("Error al enviar el mensaje: " + error.message);
+        show_alerta("Error al enviar el mensaje: " + error.message, "error");
       }
     }
   };
 
   const handleVolver = () => {
     setEnviado(false);
-    setForm({ email: "", asunto: "", descripcion: "" });
+    setForm({ email: "", asunto: "", descripcion: "" }); // Limpiar campos
   };
 
   return (
@@ -78,8 +88,8 @@ function FormContacto() {
                 </button>
               </div>
               <div className="switch-panel switch-right">
-                <h1>  Dejanos Tu Inquietud</h1>
-                <p> </p>
+                <h1>Dejanos Tu Inquietud</h1>
+                <p></p>
                 <button className="hidden btn border-white text-white w-60 fs-6" id="registrarse">
                   Enviar Comentario
                 </button>
@@ -100,42 +110,42 @@ function FormContacto() {
                   value={form.email}
                   onChange={handleChange}
                   required
-                />
+                  />
+                  </div>
+                  <div className="input-group mb-3">
+                    <input
+                      type="text"
+                      name="asunto"
+                      placeholder="Motivo"
+                      className="form-control form-control-lg bg-light fs-6"
+                      value={form.asunto}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                  <div className="input-group mb-3">
+                    <textarea
+                      className="form-control"
+                      id="exampleFormControlTextarea1"
+                      rows="10"
+                      name="descripcion"
+                      placeholder="Escribe tu mensaje aquí..."
+                      value={form.descripcion}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                  <div className="input-group mb-3 justify-content-center">
+                    <button type="submit" className="btn border-white w-60 fs-6">
+                      Enviar
+                    </button>
+                  </div>
+                </form>
               </div>
-              <div className="input-group mb-3">
-              <input
-                  type="asunto"
-                  name="asunto"
-                  placeholder="Motivo"
-                  className="form-control form-control-lg bg-light fs-6"
-                  value={form.asunto}
-                  onChange={handleChange}
-                  required
-                />
-              </div>              
-              <div className="input-group mb-3">
-                <textarea
-                  className="form-control"
-                  id="exampleFormControlTextarea1"
-                  rows="10"
-                  name="descripcion"
-                  placeholder="Escribe tu mensaje aquí..."
-                  value={form.descripcion}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-              <div className="input-group mb-3 justify-content-center">
-                <button type="submit" className="btn border-white w-60 fs-6">
-                  Enviar
-                </button>
-              </div>
-            </form>
-          </div>
+            </div>
+          )}
         </div>
-      )}
-    </div>
-  );
-}
-
-export default FormContacto;
+      );
+    }
+    
+    export default FormContacto;
